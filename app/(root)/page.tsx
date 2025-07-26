65fd8f2ab846b98d3d3f86b3015ef68068fb1323
Jackson Kaufman <jacksonkaufman115@gmail.com>
2025-07-26 16:20:38 -0600

    card built, sanity setup

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,19 @@
-import SearchForm from "../../components/SearchForm";
+import SearchForm from "@/components/SearchForm";
+import StartupCard from "@/components/StartupCard";
 
 export default async function Home({ searchParams }: { searchParams: Promise<{ query?: string }> }) {
   const query = (await searchParams).query;
+
+  const posts = [{
+    _createdAt: new Date(),
+    views: 55,
+    author: { _id: 1, name: 'Jackson Kaufman' },
+    _id: 1,
+    description: 'Description',
+    image: 'https://placehold.co/150',
+    category: 'Robots',
+    title: 'We Robots',
+   }]
 
   return (
     <>
@@ -9,6 +21,21 @@ export default async function Home({ searchParams }: { searchParams: Promise<{ q
         <h1 className="heading">Pitch Your Startup, <br /> Connect With Entrepreneurs</h1>
         <p className="sub-heading !max-w-3xl">Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions</p>
         <SearchForm query={query} />
+      </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search Results for "${query}"` : "All Startups"}
+        </p>
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post) => (
+              <StartupCard key={post._id}post={post} />
+            ))
+          ) : (
+            <p>No startups found</p>
+          )}
+        </ul>
       </section>
     </>
   );
